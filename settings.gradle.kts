f7459d8c25d2570675ed51bb980bbf829389a99f
sehoon0302 <saehun66@gmail.com>
2023-10-28 14:22:11 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SignUpUI_20231028"
+include(":app")
+ 
